1565d9c4912ee3202d604d57c0ff5ed921b16274
Velibor Markovic <veljko85@users.noreply.github.com>
2022-08-31 12:56:19 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -892,7 +892,6 @@ var createScene = function () {
           new BABYLON.ExecuteCodeAction(
             BABYLON.ActionManager.OnPickTrigger,
             function () {
-              console.log(post.material.id);
               if (post.material.id != "selectedMat" && post.scaling.x == 1) {
                 removeSideAccesories(
                   sideAccesories,
@@ -2269,6 +2268,33 @@ var createScene = function () {
             if (rightPosts[fencesArr[fenceId].parent].isVisible) {
               rightPostCaps[fencesArr[fenceId].parent].isVisible = true;
               rightPostCapClones[fencesArr[fenceId].parent].isVisible = false;
+            }
+          }
+        }
+        //for main post
+        if (fenceId > 0 && typeof activeFence == "boolean") {
+          if (type != "gardoHalf") {
+            if (leftPosts[0].scaling.z < 0.6) {
+              allWoodPosts[0].scaling.y = 1;
+              allWoodPosts[0].position.y = 0.935;
+              leftPosts[0].scaling.z = 1;
+              leftPosts[0].position.y = 0.962;
+            }
+            if (leftPosts[0].scaling.z > 0.6 && leftPosts[0].scaling.z < 0.9) {
+              allWoodPosts[0].scaling.y = 1.2;
+              allWoodPosts[0].position.y = 0.7717;
+              leftPosts[0].scaling.z = 1.2;
+              leftPosts[0].position.y = 0.7717;
+            }
+            if (leftPosts[0].scaling.z > 0.9 && leftPosts[0].scaling.z < 1) {
+              allWoodPosts[0].scaling.y = 1.475;
+              allWoodPosts[0].position.y = 0.511;
+              leftPosts[0].scaling.z = 1.475;
+              leftPosts[0].position.y = 0.511;
+            }
+            if (allPosts[0].isVisible) {
+              leftPostCaps[0].isVisible = false;
+              leftPostCapClones[0].isVisible = true;
             }
           }
         }
@@ -4162,7 +4188,7 @@ var createScene = function () {
       }
 
       //parent
-      if (h > 0) {
+      if (h > 0 && fencesArr[h].parent != undefined) {
         if (fencesArr[fencesArr[h].parent].type == "gardoHalf") {
           let fenceSibling = 0;
           for (
@@ -4210,7 +4236,19 @@ var createScene = function () {
         }
       }
 
-      if (h == 0) {
+      //for main post
+      let mainPostChildType = 0;
+      for (let i = 0; i < fencesArr.length; i++) {
+        if (
+          fencesArr[i].status == "activeFence" &&
+          fencesArr[i].parent == undefined &&
+          // fencesArr[i].type != "easyFenceHalf" &&
+          fencesArr[i].type != "gardoHalf"
+        ) {
+          mainPostChildType += 1;
+        }
+      }
+      if (mainPostChildType == 0) {
         if (leftPosts[0].scaling.z < 1.1) {
           allWoodPosts[0].scaling.y = 0.524;
           allWoodPosts[0].position.y = 0.504;
@@ -4233,10 +4271,6 @@ var createScene = function () {
           leftPostCaps[0].isVisible = false;
           leftPostCapClones[0].isVisible = true;
         }
-        // leftPostCaps[0].position.y = 0.052;
-        // leds[h].scaling.y = 0.524;
-        // leds[h].position.y = 0.5;
-        // leds[h].position.z = 0.001;
       }
     } else {
       for (let i = 8; i < fenceBoards[h].length; i++) {
@@ -4336,9 +4370,35 @@ var createScene = function () {
           rightPostCaps[fencesArr[h].parent].isVisible = true;
           rightPostCapClones[fencesArr[h].parent].isVisible = false;
         }
-        // ledsRight[fencesArr[h].parent].scaling.z = 1;
-        // ledsRight[fencesArr[h].parent].position.z = 0;
-        // ledsRight[fencesArr[h].parent].position.y = 0.001;
+      }
+      //for main post
+      if (
+        h > 0 &&
+        fencesArr[h].parent == undefined &&
+        fencesArr[h].status == "activeFence"
+      ) {
+        if (leftPosts[0].scaling.z < 0.6) {
+          allWoodPosts[0].scaling.y = 1;
+          allWoodPosts[0].position.y = 0.935;
+          leftPosts[0].scaling.z = 1;
+          leftPosts[0].position.y = 0.962;
+        }
+        if (leftPosts[0].scaling.z > 0.6 && leftPosts[0].scaling.z < 0.9) {
+          allWoodPosts[0].scaling.y = 1.2;
+          allWoodPosts[0].position.y = 0.7717;
+          leftPosts[0].scaling.z = 1.2;
+          leftPosts[0].position.y = 0.7717;
+        }
+        if (leftPosts[0].scaling.z > 0.9 && leftPosts[0].scaling.z < 1) {
+          allWoodPosts[0].scaling.y = 1.475;
+          allWoodPosts[0].position.y = 0.5117;
+          leftPosts[0].scaling.z = 1.475;
+          leftPosts[0].position.y = 0.511;
+        }
+        if (leftPosts[0].isVisible) {
+          leftPostCaps[0].isVisible = true;
+          leftPostCapClones[0].isVisible = false;
+        }
       }
     }
     //set rank to ohne
@@ -4497,6 +4557,42 @@ var createScene = function () {
         // ledsRight[fencesArr[a].parent].scaling.z = 0.524;
         // ledsRight[fencesArr[a].parent].position.z = 0.46;
         // ledsRight[fencesArr[a].parent].position.y = 0.001;
+      }
+    }
+    //for main post
+    let mainPostChildType = 0;
+    for (let i = 0; i < fencesArr.length; i++) {
+      if (
+        fencesArr[i].status == "activeFence" &&
+        fencesArr[i].parent == undefined &&
+        // fencesArr[i].type != "easyFenceHalf" &&
+        fencesArr[i].type != "gardoHalf"
+      ) {
+        mainPostChildType += 1;
+      }
+    }
+    if (mainPostChildType == 0) {
+      if (leftPosts[0].scaling.z < 1.1) {
+        allWoodPosts[0].scaling.y = 0.524;
+        allWoodPosts[0].position.y = 0.504;
+        leftPosts[0].scaling.z = 0.574;
+        leftPosts[0].position.y = 0.554;
+      }
+      if (leftPosts[0].scaling.z > 1 && leftPosts[0].scaling.z < 1.4) {
+        allWoodPosts[0].scaling.y = 0.724;
+        allWoodPosts[0].position.y = 0.41;
+        leftPosts[0].scaling.z = 0.724;
+        leftPosts[0].position.y = 0.41;
+      }
+      if (leftPosts[0].scaling.z > 1.4) {
+        allWoodPosts[0].scaling.y = 0.999;
+        allWoodPosts[0].position.y = 0.15;
+        leftPosts[0].scaling.z = 0.999;
+        leftPosts[0].position.y = 0.15;
+      }
+      if (leftPosts[0].isVisible) {
+        leftPostCaps[0].isVisible = false;
+        leftPostCapClones[0].isVisible = true;
       }
     }
     //
